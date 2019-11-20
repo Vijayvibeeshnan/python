b529817f691b826a077447fe3866a9d34ef3d1be
Vijayvibeeshnan <54533948+Vijayvibeeshnan@users.noreply.github.com>
2019-11-20 11:03:55 +0530

    Add files via upload

---
+++ source to distination.py
@@ -0,0 +1,18 @@
+Name=input("Enter the name:")
+print(Name)
+Email=input("Enter the email:")
+print(Email)
+Phone=int(input("Enter phone no:"))
+print(Phone)
+print("Bus=15km/hr","\nCar=20km/hr","\nBike=25km\hr")
+Choice=("Bus \nCar \nBike")
+print("Bus \nCar \nBike")
+Ch=input("choose the choice:")
+if Ch=="Bus":
+    print("Bus=150")
+if Ch=="Car":
+    print("Car=100")
+if Ch=="Bike":
+    print("Bike=50")
+else:
+    print("choose correct option")
