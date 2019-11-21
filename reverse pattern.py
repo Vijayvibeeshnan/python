15f888e9e8b38709d77635cf10f2f4155bbee0da
Vijayvibeeshnan <54533948+Vijayvibeeshnan@users.noreply.github.com>
2019-11-21 13:47:08 +0530

    Update and rename rev pattern.py to reverse pattern.py

--- reverse pattern.py
+++ reverse pattern.py
@@ -3,8 +3,3 @@ for i in range(n,0,-1):
     for j in range(i):
         print("*",end="")
     print()
-for k in range(1,n):
-    for m in range(0,k+1):
-        print("*",end="")
-    print()
-    
