d10b150f51fbac43d7132aca8dd32e8880120a97
Vijayvibeeshnan <54533948+Vijayvibeeshnan@users.noreply.github.com>
2019-11-21 20:12:50 +0530

    Add files via upload

---
+++ for break&con.py
@@ -0,0 +1,12 @@
+#continue use for
+n=int(input("Enter the no:"))
+for i in range(n):
+    if i==0:
+        continue
+    print(i)
+    
+#break
+for i in range (n):
+    print(i)
+    if i==3:
+        break
